421f310551bb73ccb830ef5f11a5e248f68166a7
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-07-12 02:18:02 -0300

    final commit

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
 var websocket;
 var websocketInput = false;
+var bookmark;
 
 function setupWebsocket() {
     var wsUri = "ws://" + document.location.host + document.location.pathname + "endpoint";
@@ -15,7 +16,10 @@ function setupWebsocket() {
 
     websocket.onmessage = function (evt) {
         console.log("WEBSOCKET: message", evt.data);
-        tinyMCE.get('editor').setContent(evt.data);
+        tinyMCE.get('editor').setContent(evt.data, {
+            format: 'raw'
+        });
+        tinyMCE.get('editor').selection.moveToBookmark(bookmark);
         websocketInput = false;
     };
 
@@ -29,6 +33,7 @@ function editorChanged(e) {
     if (!websocketInput) {
         console.log('DEBUG: editorChanged');
         var editorHTML = tinyMCE.get('editor').getContent();
+        bookmark = tinyMCE.get('editor').selection.getBookmark(2, true);
         console.log('DEBUG:', editorHTML);
         websocketInput = true;
         websocket.send(editorHTML);
